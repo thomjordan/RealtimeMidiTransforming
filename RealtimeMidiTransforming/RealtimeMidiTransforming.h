f378de63dbf11926b3d55740052624299ef298de
ThomJordan <thomjordan@users.noreply.github.com>
2019-03-17 21:35:37 -0700

    Initial Commit

---
+++ RealtimeMidiTransforming/RealtimeMidiTransforming.h
@@ -0,0 +1,19 @@
+//
+//  RealtimeMidiTransforming.h
+//  RealtimeMidiTransforming
+//
+//  Created by Thom Jordan on 3/17/19.
+//  Copyright © 2019 Thom Jordan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RealtimeMidiTransforming.
+FOUNDATION_EXPORT double RealtimeMidiTransformingVersionNumber;
+
+//! Project version string for RealtimeMidiTransforming.
+FOUNDATION_EXPORT const unsigned char RealtimeMidiTransformingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealtimeMidiTransforming/PublicHeader.h>
+
+
